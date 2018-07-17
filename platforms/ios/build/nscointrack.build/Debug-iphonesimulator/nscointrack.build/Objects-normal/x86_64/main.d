a79153aba59c4858d3322bb6a40c63809832693f
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-07-17 22:54:46 +0300

    initial commit

---
+++ platforms/ios/build/nscointrack.build/Debug-iphonesimulator/nscointrack.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,11 @@
+dependencies: \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/nscointrack/nscointrack-Prefix.pch \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/main.m \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/NativeScript.framework/Headers/NativeScript.h \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Diagnostics.h \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime.h \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntime+Inspector.h \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/NativeScript.framework/Headers/TNSRuntimeInstrumentation.h \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/TNSExceptionHandler.h \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/TNSDebugging.h \
+  /Users/ivan/Documents/apps/SoftUni/NativeScriptDevelopment/ns-coin-track/platforms/ios/internal/TKLiveSync/include/TKLiveSync.h
