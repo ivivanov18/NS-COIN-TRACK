dd6c20b25608a1d7258a6b0cf3737bef5019b8fc
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-07-30 23:23:01 +0300

    feat: list view works with hard coded list of coins

--- platforms/ios/nscointrack/app/main-page.js
+++ platforms/ios/nscointrack/app/main-page.js
@@ -8,28 +8,42 @@ logic, and to set up your page’s data binding.
 NativeScript adheres to the CommonJS specification for dealing with
 JavaScript modules. The CommonJS require() function is how you import
 JavaScript modules defined in other files.
-*/ 
-var createViewModel = require("./main-view-model").createViewModel;
+*/
+
+//const createViewModel = require("./main-view-model").createViewModel;
+const ObservableArray = require("data/observable-array").ObservableArray;
+const Observable = require("data/observable").Observable;
+
+const cryptoCurrencies = new ObservableArray([]);
+const pageData = new Observable();
 
 function onNavigatingTo(args) {
-    /*
-    This gets a reference this page’s <Page> UI component. You can
-    view the API reference of the Page to see what’s available at
-    https://docs.nativescript.org/api-reference/classes/_ui_page_.page.html
-    */
-    var page = args.object;
+  const page = args.object;
+  page.bindingContext = pageData;
 
-    /*
-    A page’s bindingContext is an object that should be used to perform
-    data binding between XML markup and JavaScript code. Properties
-    on the bindingContext can be accessed using the {{ }} syntax in XML.
-    In this example, the {{ message }} and {{ onTap }} bindings are resolved
-    against the object returned by createViewModel().
-
-    You can learn more about data binding in NativeScript at
-    https://docs.nativescript.org/core-concepts/data-binding.
-    */
-    page.bindingContext = createViewModel();
+  cryptoCurrencies.push(
+    {
+      coinName: "Bitcoin",
+      coinSign: "BTC",
+      itemImage: ""
+    },
+    {
+      coinName: "Ethereum",
+      coinSign: "ETH",
+      itemImage: ""
+    },
+    {
+      coinName: "XRP",
+      coinSign: "Ripple",
+      itemImage: ""
+    },
+    {
+      coinName: "Litecoin",
+      coinSign: "LTC",
+      itemImage: ""
+    }
+  );
+  pageData.set("coins", cryptoCurrencies);
 }
 
 /*
