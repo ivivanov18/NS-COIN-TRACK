528adc2d499c813771ff87648674b5155bfab5d3
ivivanov18 <ivanov.ivan.v@gmail.com>
2018-08-05 00:37:39 +0300

    feat: added simple ListView instead of RadListView for favoriteCoinsList because RadListView did not update with observableArray
    feat: when tapped on star the star correctly changes to full star to show that coin is a favorite, correctly update the favoritelistCoins in the according tab
    
    refactor: added function findIndexElementOf to find according index of given element in the given array --> willbe used by both coinsList and favCoinsList

--- app/main-view-model.js
+++ app/main-view-model.js
@@ -1,47 +1,71 @@
 const ObservableArray = require("data/observable-array").ObservableArray;
 const Observable = require("data/observable").Observable;
 
-const cryptoCurrencies = [];
+const cryptoCurrencies = new ObservableArray([
+  {
+    coinName: "Bitcoin",
+    coinSign: "BTC",
+    itemImage: "",
+    isFavorite: true
+  },
+  {
+    coinName: "Ethereum",
+    coinSign: "ETH",
+    itemImage: "",
+    isFavorite: false
+  },
+  {
+    coinName: "XRP",
+    coinSign: "Ripple",
+    itemImage: "",
+    isFavorite: false
+  },
+  {
+    coinName: "Litecoin",
+    coinSign: "LTC",
+    itemImage: "",
+    isFavorite: false
+  }
+]);
 
-function createViewModel() {
-  var viewModel = new Observable();
-  //var cryptoCurrencies = new ObservableArray([]);
+const findIndexOfElement = (array, coinSign) => {
+  let indexOfElementToUpdate = -1;
 
-  cryptoCurrencies.push(
-    {
-      coinName: "Bitcoin",
-      coinSign: "BTC",
-      itemImage: "",
-      isFavorite: false
-    },
-    {
-      coinName: "Ethereum",
-      coinSign: "ETH",
-      itemImage: "",
-      isFavorite: false
-    },
-    {
-      coinName: "XRP",
-      coinSign: "Ripple",
-      itemImage: "",
-      isFavorite: false
-    },
-    {
-      coinName: "Litecoin",
-      coinSign: "LTC",
-      itemImage: "",
-      isFavorite: false
+  array.forEach((element, index) => {
+    if (element.coinSign === coinSign) {
+      indexOfElementToUpdate = index;
+      return;
     }
-  );
+  });
 
+  return indexOfElementToUpdate;
+};
+
+const getFavoriteCoins = array => {};
+
+const createViewModel = () => {
+  var viewModel = new Observable();
+
+  //viewModel.set("coinsList", cryptoCurrencies);
   viewModel.coinsList = cryptoCurrencies;
+  const filteredFavorites = viewModel.coinsList.filter(
+    element => element.isFavorite
+  );
+  viewModel.favoriteCoinsList = new ObservableArray(filteredFavorites);
 
   viewModel.onTapStar = function(args) {
     const coinSign = args.object.coin;
-    console.log("TAPPED: ", coinSign);
+
+    let indexOfElementToUpdate = findIndexOfElement(this.coinsList, coinSign);
+
+    const modifiedItem = this.coinsList.getItem(indexOfElementToUpdate);
+    modifiedItem.isFavorite = !modifiedItem.isFavorite;
+    this.favoriteCoinsList.push(modifiedItem);
+    console.log("this.favoriteCoinsList: ", this.favoriteCoinsList);
+    this.coinsList.setItem(indexOfElementToUpdate, modifiedItem);
   };
 
   return viewModel;
-}
+};
 
 exports.createViewModel = createViewModel;
